3e0681ee33992f1e37376fbb7dab7e2d59b8a698
Victor Gabriel <victorgabdev@gmail.com>
2023-07-13 17:33:28 -0300

    Game Seed

--- src/main/java/com/dslist/games/entities/Game.java
+++ src/main/java/com/dslist/games/entities/Game.java
@@ -16,21 +16,25 @@ public class Game {
     @Column(name = "game_year") // "year" é um nome reservado do sql
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {}
 
-    public Game(Long id, String title, Integer year, String genre, String platform,
+    public Game(Long id, String title, Integer year, String genre, String platforms,
                 Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -70,11 +74,11 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
