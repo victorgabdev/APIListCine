0aba222aaae9606e46c7cfc8db47a81ccd1834d3
Victor Gabriel <victorgabdev@gmail.com>
2023-07-13 16:54:22 -0300

    Project created

---
+++ src/test/java/com/dslist/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dslist.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
